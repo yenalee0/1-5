480b1a41e8675fe2834b9381357900d7e71dc109
yenalee0 <2553s2610526@g.cnees.kr>
2026-05-29 13:21:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 import streamlit as st
-st.title('나는 착하다')
-st.write('바이브코딩 재미있다!!')
-st.write('나는 배구를 잘한다')
+import random
+
+# 메뉴 리스트
+foods = [
+    "김치찌개",
+    "제육볶음",
+    "돈까스",
+    "햄버거",
+    "치킨",
+    "떡볶이",
+    "초밥",
+    "파스타",
+    "마라탕",
+    "삼겹살"
+]
+
+# 제목
+st.title("🍚 오늘 뭐 먹지?")
+
+st.write("버튼을 누르면 메뉴를 추천해줘요!")
+
+# 버튼
+if st.button("추천 받기"):
+    food = random.choice(foods)
+    st.success(f"오늘의 추천 메뉴는 👉 {food}")
